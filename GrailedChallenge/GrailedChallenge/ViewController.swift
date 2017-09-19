dc52ffd94772ea25ff6b53e0dcd8f370e90106c2
Phillip English <vitkiarts@gmail.com>
2017-09-19 11:54:23 -0400

    Add project files and pods

---
+++ GrailedChallenge/GrailedChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrailedChallenge
+//
+//  Created by Phillip English on 9/19/17.
+//  Copyright © 2017 vitkiarts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
