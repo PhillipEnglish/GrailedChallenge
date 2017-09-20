e557f48b64edc3ba40ad21748abd7cb2343a22f5
Phillip English <vitkiarts@gmail.com>
2017-09-19 21:46:34 -0400

    Add Constants file

---
+++ GrailedChallenge/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GrailedChallenge
+//
+//  Created by Phillip English on 9/19/17.
+//  Copyright © 2017 vitkiarts. All rights reserved.
+//
+
+import Foundation
+
+internal struct Constants {
+	static let baseURLString = "https://www.grailed.com/"
+	static let cdnURLString = "https://cdn.fs.grailed.com/AJdAgngCST4iPtnUxiGtTz/rotate=deg:exif/rotate=deg:0/resize=width:"
+	
+}
