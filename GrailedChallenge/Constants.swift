301e4dca5ae815a88b896ca69e4696428437264a
Phillip English <vitkiarts@gmail.com>
2017-09-24 19:48:32 -0400

    Add path strings to constants

--- GrailedChallenge/Constants.swift
+++ GrailedChallenge/Constants.swift
@@ -11,5 +11,7 @@ internal struct Constants {
 	static let baseURLString = "https://www.grailed.com/api/"
 	static let baseCDNString = "https://cdn.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/rotate=deg:exif/rotate=deg:0/resize=width:"
 	static let secondaryCDNString = ",fit:crop/output=format:jpg,compress:true,quality:95/"
+	static let articlesPathString = "articles/ios_index"
+	static let savedSearchPathString = "merchandise/marquee"
 	
 }
