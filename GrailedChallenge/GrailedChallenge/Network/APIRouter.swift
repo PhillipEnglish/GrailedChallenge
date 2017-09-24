7b495b59f0a7b96341fd3ed18a3e1714e9e69d01
Phillip English <vitkiarts@gmail.com>
2017-09-24 19:09:54 -0400

    Add APIRouter file

---
+++ GrailedChallenge/GrailedChallenge/Network/APIRouter.swift
@@ -0,0 +1,16 @@
+//
+//  APIRouter.swift
+//  GrailedChallenge
+//
+//  Created by Phillip English on 9/24/17.
+//  Copyright © 2017 vitkiarts. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum APIRouter: URLRequestConvertible {
+	
+	case get
+	
+}
