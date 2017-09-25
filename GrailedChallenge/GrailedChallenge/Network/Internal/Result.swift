0f8d5a83b34c085a1463a7cbb177cb6cdeb2591a
Phillip English <vitkiarts@gmail.com>
2017-09-24 21:28:41 -0400

    Add Result Enum
    Create APIController file

---
+++ GrailedChallenge/GrailedChallenge/Network/Internal/Result.swift
@@ -0,0 +1,23 @@
+//
+//  Result.swift
+//  GrailedChallenge
+//
+//  Created by Phillip English on 9/24/17.
+//  Copyright © 2017 vitkiarts. All rights reserved.
+//
+
+import Foundation
+
+public enum Result<T> {
+	case success(T)
+	case failure(Error)
+	
+	var error: Error? {
+		switch self {
+		case .success(_): return nil
+		case .failure(let error): return error
+		}
+	}
+	
+	
+}
