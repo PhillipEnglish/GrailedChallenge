63965da79fcda0babad2f4d434c8f7de7f0d0640
Phillip English <vitkiarts@gmail.com>
2017-09-26 12:56:54 -0400

    Remove old APIController methods. Add new getArticles method

--- GrailedChallenge/GrailedChallenge/Network/APIController.swift
+++ GrailedChallenge/GrailedChallenge/Network/APIController.swift
@@ -8,26 +8,37 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
-typealias FeedCompletionHandler = (Result<Data>) -> Void
+typealias FeedCompletionHandler = (Result<JSON>) -> Void
 
 struct APIController {
-	func getArticles(pagination: String?, completionHandler: @escaping FeedCompletionHandler) {
-		let _ = Alamofire.request(APIRouter.getArticles(pagination)).responseData { response in
-			guard response.result.error == nil else {
-				if let error = response.result.error {
-					completionHandler(Result.failure(error))
-				}
+	
+	func getArticles(pagination: String?, compleionHandler: @escaping FeedCompletionHandler) {
+		let page: String = pagination ?? ""
+		let endpoint = Constants.baseURLString + Constants.articlesPathString + page
+		guard let url = URL(string: endpoint) else { return }
+		
+		let urlRequest = URLRequest(url: url)
+		let session = URLSession.shared
+		
+		let task = session.dataTask(with: urlRequest) { data, response, error in
+			
+			guard let responseData = data else {
+				compleionHandler(Result.failure(GrailedError.invalidDataResponse))
 				return
 			}
 			
-			if let data = response.data {
-			completionHandler(Result.success(data))
+			let responseCode = response?.getStatusCode()
+			guard responseCode == 200 else {
+				compleionHandler(Result.failure(GrailedError.invalidResponseCode(responseCode)))
 			}
+			
+			let responseJSON = JSON(responseData)
+			
+			compleionHandler(Result.success(responseJSON))
 		}
-		
-		
+		task.resume()
 	}
-	
 	
 }
