7733648fe510a4fc6ed9fec383affa887c2e8e13
Phillip English <vitkiarts@gmail.com>
2017-09-27 09:42:39 -0400

    Implement viewmodel in ArticlesVC

--- GrailedChallenge/GrailedChallenge/Views/ArticlesViewController.swift
+++ GrailedChallenge/GrailedChallenge/Views/ArticlesViewController.swift
@@ -12,27 +12,30 @@ class ArticlesViewController: UIViewController {
 	
 	@IBOutlet weak var articlesTableView: UITableView!
 	
+	// MARK: - Properties
+	lazy var articlesViewModel: ArticlesViewModel = {
+		let viewModel: ArticlesViewModel = ArticlesViewModel()
+		viewModel.delegate = self
+		return viewModel
+	}
+	
+	let progressHUD = ProgressHUD(text: "Loading")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		articlesViewModel.getArticlesFromServer(pagination: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	extension ArticlesViewController: UITableViewDataSource, UITableViewDelegate {
+		func numberOfSections(in tableView: UITableView) -> Int {
+			return 1
+		}
+		
+		func tableView(
+	}
 }
